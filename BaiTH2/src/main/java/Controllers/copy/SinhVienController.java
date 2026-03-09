00ca72f3b628999a846e3ece7e322ed3cfae1044
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-03-09 21:32:09 +0700

    update sinhviencontroller.java

--- BaiTH2/src/main/java/Controllers/copy/SinhVienController.java
+++ BaiTH2/src/main/java/Controllers/copy/SinhVienController.java
@@ -1,4 +1,4 @@
-package Controllers.copy; // Đảm bảo đúng tên package của bạn
+package Controllers.copy; 
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model; // Chú ý import đúng class Model này
@@ -10,13 +10,13 @@ public class SinhVienController {
     @GetMapping("/thongtin")
     public String hienThiThongTin(Model model) {
         
-        // 1. Dùng model.addAttribute("tên_biến", "giá_trị") để truyền dữ liệu
+        
         model.addAttribute("mssv", "65131508");
         model.addAttribute("hoTen", "Cái Trần Đăng Khôi");
         model.addAttribute("namSinh", 2005); 
         model.addAttribute("gioiTinh", "Nam");
 
-        // 2. Trả về tên của file giao diện (không cần đuôi .html)
+        
         return "thongtin"; 
     }
 }
