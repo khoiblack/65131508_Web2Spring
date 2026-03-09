6094bbba9626794919e6c60cd5479623b49a0a03
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-03-09 13:56:14 +0700

    B1_HelloSpringBoot

---
+++ BaiTH1/src/main/java/com/example/demo/WebController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// Đánh dấu đây là một Controller
+// Nơi tiếp nhận các reqquest từ phía người dùng
+@Controller
+public class WebController {
+
+    // Đón nhận request GET
+    @GetMapping("/") // Nếu người dùng request tới địa chỉ "/"
+    public String index() {
+        return "index"; // Trả về file index.html
+    }
+}
