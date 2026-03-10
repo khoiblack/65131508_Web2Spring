ceeb5d53d814c3d05709b4bb934a95ddae8f9c6f
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-03-10 14:06:14 +0700

    code file java bài 3

--- BaiTH2/src/main/java/com/example/demo/B3_LayThamSoTaiController.java
+++ BaiTH2/src/main/java/com/example/demo/B3_LayThamSoTaiController.java
@@ -1,5 +1,18 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class B3_LayThamSoTaiController {
+	@GetMapping("/bai3")
+	public String welcome(@RequestParam(name = "name", required = false, defaultValue = "Khách") String hoTen,
+			ModelMap model) {
+		model.addAttribute("msg", "Chào mừng " + hoTen + " đến với Spring Boot!");
+		return "bai3";
+	}
 
 }
+
