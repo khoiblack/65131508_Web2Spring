c28c5664c03ebebd988f1591957dd4cb9956805d
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-03-10 13:43:45 +0700

    update file

--- BaiTH2/src/main/java/com/example/demo/B1_HelloSpringBoot.java
+++ BaiTH2/src/main/java/com/example/demo/B1_HelloSpringBoot.java
@@ -1,22 +1,15 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model; 
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class B1_HelloSpringBoot {
 
-    @GetMapping("/bai2")
-    public String hienThiThongTin(Model model) {
-        
-        
-        model.addAttribute("mssv", "65131508");
-        model.addAttribute("hoTen", "Cái Trần Đăng Khôi");
-        model.addAttribute("namSinh", 2005); 
-        model.addAttribute("gioiTinh", "Nam");
-
-        
-        return "bai2"; 
+    
+    @GetMapping("/bai1") 
+    public String index() {
+        return "bai1"; 
     }
 }
