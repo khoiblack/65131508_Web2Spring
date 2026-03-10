d5cb70e9453a898b9611a94b964cb9e6d89962d1
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-03-10 21:39:50 +0700

    update code model

---
+++ BaiTH2/src/main/java/khoi/thSrpingBoot/Model/SanPham.java
@@ -0,0 +1,35 @@
+package khoi.thSrpingBoot.Model;
+
+
+
+public class SanPham {
+    private String maSP;
+    private String tenSP;
+    private double gia;
+    private int soLuong;
+
+    
+    public SanPham() {
+    }
+
+    
+    public SanPham(String maSP, String tenSP, double gia, int soLuong) {
+        this.maSP = maSP;
+        this.tenSP = tenSP;
+        this.gia = gia;
+        this.soLuong = soLuong;
+    }
+
+    
+    public String getMaSP() { return maSP; }
+    public void setMaSP(String maSP) { this.maSP = maSP; }
+
+    public String getTenSP() { return tenSP; }
+    public void setTenSP(String tenSP) { this.tenSP = tenSP; }
+
+    public double getGia() { return gia; }
+    public void setGia(double gia) { this.gia = gia; }
+
+    public int getSoLuong() { return soLuong; }
+    public void setSoLuong(int soLuong) { this.soLuong = soLuong; }
+}
