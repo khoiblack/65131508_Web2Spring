25d2145a834ecd8307eb0605ad21764693ede5c3
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-03-11 11:33:41 +0700

    finish bài 5

--- BaiTH2/src/main/java/khoi/thSrpingBoot/Controllers/B5_TruyenDoiTuong2View.java
+++ BaiTH2/src/main/java/khoi/thSrpingBoot/Controllers/B5_TruyenDoiTuong2View.java
@@ -1,7 +1,5 @@
 package khoi.thSrpingBoot.Controllers;
 
-
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +12,10 @@ public class B5_TruyenDoiTuong2View {
     @GetMapping("/bai5")
     public String hienThiSanPham(Model model) {
         
-        
         SanPham sp1 = new SanPham("LP001", "Laptop Dell XPS 15", 25500000.0, 10);
-        
         
         model.addAttribute("sp", sp1);
         
-        return "view-san-pham"; 
+        return "bai5"; 
     }
 }
