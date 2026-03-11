327b0d653d20bfe121e91bccdacb4d30d0f38ee3
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-03-11 16:37:48 +0700

    hard-code dữ liệu của topic

---
+++ ntu_cms/src/main/java/thiGK/ntu65131508caitrandangkhoi_fitcms/models/ExampleData.java
@@ -0,0 +1,16 @@
+package thiGK.ntu65131508caitrandangkhoi_fitcms.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExampleData {
+    
+    public static List<Topic> topics = new ArrayList<>();
+
+    
+    static {
+        topics.add(new Topic("T01", "Website Bán Hàng", "Phát triển web E-commerce", "GV01", "Web"));
+        topics.add(new Topic("T02", "App Quản Lý Sinh Viên", "Viết ứng dụng Android", "GV02", "Mobile"));
+        topics.add(new Topic("T03", "AI Nhận Diện Khuôn Mặt", "Nghiên cứu Machine Learning", "GV03", "AI"));
+    }
+}
