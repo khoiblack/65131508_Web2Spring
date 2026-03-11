aa56bb6928b668ae2ff8280f7092c12e0dabbc89
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-03-11 15:08:45 +0700

    update code

---
+++ ntu_cms/src/main/java/thiGK/ntu65131508caitrandangkhoi_fitcms/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package thiGK.ntu65131508caitrandangkhoi_fitcms.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String dashBoard() {
+		return "home";
+	}
+}
