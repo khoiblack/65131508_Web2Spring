70c2726b78219ea3695cde9b5d5d282a6f4c1641
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-03-11 16:38:11 +0700

    tạo model của topic

---
+++ ntu_cms/src/main/java/thiGK/ntu65131508caitrandangkhoi_fitcms/models/Topic.java
@@ -0,0 +1,35 @@
+package thiGK.ntu65131508caitrandangkhoi_fitcms.models;
+
+public class Topic {
+    private String id;
+    private String topicName;
+    private String topicDescription;
+    private String supervisorId;
+    private String topicType;
+
+    public Topic() {
+    }
+
+    public Topic(String id, String topicName, String topicDescription, String supervisorId, String topicType) {
+        this.id = id;
+        this.topicName = topicName;
+        this.topicDescription = topicDescription;
+        this.supervisorId = supervisorId;
+        this.topicType = topicType;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getTopicName() { return topicName; }
+    public void setTopicName(String topicName) { this.topicName = topicName; }
+
+    public String getTopicDescription() { return topicDescription; }
+    public void setTopicDescription(String topicDescription) { this.topicDescription = topicDescription; }
+
+    public String getSupervisorId() { return supervisorId; }
+    public void setSupervisorId(String supervisorId) { this.supervisorId = supervisorId; }
+
+    public String getTopicType() { return topicType; }
+    public void setTopicType(String topicType) { this.topicType = topicType; }
+}
