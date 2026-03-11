7406bad1095f5ebd851a588a32ca8fd3560862c3
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-03-11 14:38:12 +0700

    bài thi thử giữa kỳ

---
+++ ntu_cms/src/main/java/thiGK/ntu65131508caitrandangkhoi_fitcms/NtuCmsApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu65131508caitrandangkhoi_fitcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtuCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtuCmsApplication.class, args);
+	}
+
+}
