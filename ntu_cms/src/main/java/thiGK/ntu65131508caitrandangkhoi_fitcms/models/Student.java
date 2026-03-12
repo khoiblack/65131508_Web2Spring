0ddc73ee19dd3dbc9bdda25036d97d9f1351f5e1
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-03-12 10:33:48 +0700

    update code

--- ntu_cms/src/main/java/thiGK/ntu65131508caitrandangkhoi_fitcms/models/Student.java
+++ ntu_cms/src/main/java/thiGK/ntu65131508caitrandangkhoi_fitcms/models/Student.java
@@ -1,5 +1,27 @@
 package thiGK.ntu65131508caitrandangkhoi_fitcms.models;
 
-public class Student {
 
+
+public class Student {
+    private String id;
+    private String name;
+    private String groupId;
+
+    public Student() {
+    }
+
+    public Student(String id, String name, String groupId) {
+        this.id = id;
+        this.name = name;
+        this.groupId = groupId;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getGroupId() { return groupId; }
+    public void setGroupId(String groupId) { this.groupId = groupId; }
 }
