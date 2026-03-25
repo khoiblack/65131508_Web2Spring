e653078b831432e0a5666a9663ea2b373aba58e3
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-03-25 14:35:37 +0700

    tạo code controller

---
+++ DemoBaoMatCoBan/src/main/java/ntu/khoi/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package ntu.khoi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
