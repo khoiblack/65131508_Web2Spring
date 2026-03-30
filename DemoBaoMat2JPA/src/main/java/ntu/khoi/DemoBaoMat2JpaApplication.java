24011b7b49a1df39cf916832020b0eaa8deffa77
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-03-30 14:32:40 +0700

    tạo bài thực hành

---
+++ DemoBaoMat2JPA/src/main/java/ntu/khoi/DemoBaoMat2JpaApplication.java
@@ -0,0 +1,13 @@
+package ntu.khoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBaoMat2JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBaoMat2JpaApplication.class, args);
+	}
+
+}
