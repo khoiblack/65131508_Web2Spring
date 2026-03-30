6bd167b8cf94d7a2645b04b7a9f952711c15c84e
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-03-30 14:33:49 +0700

    tạo code entity role

---
+++ DemoBaoMat2JPA/src/main/java/ntu/khoi/models/Role.java
@@ -0,0 +1,33 @@
+package ntu.khoi.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String name;  // Ví dụ: ROLE_USER, ROLE_ADMIN
+//getter, setter, contrstructor
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+    
+}
