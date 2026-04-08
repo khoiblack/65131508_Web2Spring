77d9c66e6f4028c4a171d2bc6b15ef09c7b87ebc
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-04-08 10:15:02 +0700

    update code

--- SessiomwithSpringSecurity/src/main/java/ntu/khoi/controllers/HomeController.java
+++ SessiomwithSpringSecurity/src/main/java/ntu/khoi/controllers/HomeController.java
@@ -1,22 +1,22 @@
 package ntu.khoi.controllers;
 
-
-
-
-
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/home")
-    public String home() {
+    public String home(Authentication authentication, ModelMap m) {
+        // Kiểm tra xem có ai đang đăng nhập không
+        if (authentication != null) {
+            // Lấy tên tài khoản nhét vào cái biến tên là "au" để gửi ra màn hình
+            m.addAttribute("au", authentication.getName());
+        }
         return "home";
     }
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
+    // Đã xóa hàm @GetMapping("/login") vì chúng ta dùng form mặc định của Spring
 }
