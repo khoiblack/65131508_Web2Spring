e8668d9136f1b49155d101429ca5c215426a2cb2
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-04-24 13:46:02 +0700

    thi gk2

---
+++ ThiGiuaKy2/src/main/java/thigk2/caitrandangkhoi/ThiGiuaKy2Application.java
@@ -0,0 +1,13 @@
+package thigk2.caitrandangkhoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiGiuaKy2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThiGiuaKy2Application.class, args);
+	}
+
+}
---
+++ ThiGiuaKy2/src/test/java/thigk2/caitrandangkhoi/ThiGiuaKy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package thigk2.caitrandangkhoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThiGiuaKy2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
