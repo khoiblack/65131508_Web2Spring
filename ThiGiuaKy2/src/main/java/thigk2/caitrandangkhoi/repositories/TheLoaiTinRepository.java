f00ec52f665b80d878c01845bac4ac1e83f8629d
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-04-24 14:18:19 +0700

    tạo TheLoaiRepository

---
+++ ThiGiuaKy2/src/main/java/thigk2/caitrandangkhoi/repositories/TheLoaiTinRepository.java
@@ -0,0 +1,7 @@
+package thigk2.caitrandangkhoi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import thigk2.caitrandangkhoi.models.TheLoaiTin;
+
+public interface TheLoaiTinRepository extends JpaRepository<TheLoaiTin, Integer> {}
